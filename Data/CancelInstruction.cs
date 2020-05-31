7055c0f26a9027a2983fdeceb6faaef73f3e03ea
betvader <bet.vader@yahoo.com>
2020-05-31 13:31:30 +1000

    fix: cancel instruction requires to be set as null to do a full cancel

--- Data/CancelInstruction.cs
+++ Data/CancelInstruction.cs
@@ -12,7 +12,11 @@ namespace BetfairNG.Data
         [JsonProperty(PropertyName = "betId")]
         public string BetId { get; set; }
 
+        /// <summary>
+        /// https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/Betting+Enums#BettingEnums-InstructionReportStatus
+        /// https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/Betting+Type+Definitions#BettingTypeDefinitions-CancelInstruction see size reduction field
+        /// </summary>
         [JsonProperty(PropertyName = "sizeReduction")]
-        public double SizeReduction { get; set; }
+        public double? SizeReduction { get; set; }
     }
 }
